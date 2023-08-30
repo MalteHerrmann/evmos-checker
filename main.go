075c5a4d8698e3a60d009f5f725b67df91e59658
MalteHerrmann <malte@evmos.org>
2023-08-30 23:24:37 +0200

    run cobra-cli init

---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+Evmos Checker | Malte Herrmann | Evmos Core Team
+
+This tool is a collection of checks that are run during development
+of the Evmos core protocol.
+*/
+package main
+
+import "github.com/MalteHerrmann/evmos-checks/cmd"
+
+func main() {
+	cmd.Execute()
+}
