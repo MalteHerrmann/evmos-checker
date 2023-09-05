45653c66366e1af8e85e2073e3286a576c675328
MalteHerrmann <malte@evmos.org>
2023-09-06 01:15:27 +0200

    address linters

--- checks/lint/lint.go
+++ checks/lint/lint.go
@@ -4,30 +4,35 @@ Package lint contains the various linter checks.
 package lint
 
 import (
-	"fmt"
-
+	"github.com/MalteHerrmann/evmos-checker/checker"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
-// LintCmd represents the CLI command for linting.
-var LintCmd = &cobra.Command{
-	Use:   "lint",
-	Short: "Runs the linter checks",
-	Long: `Runs the linter checks.
+// lintShortDesc is the long description of the linting sub-checks.
+const lintLongDesc = `Runs the linter checks.
 
 Included linters are:
  - gofumpt
  - golangci-lint
  - markdownlint
- - markdown-link-check`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("lint called")
-	},
-}
+ - markdown-link-check`
 
 // NewLintCmd returns the root command of the evmos-checker linting subchecks.
 func NewLintCmd() *cobra.Command {
-	// Define flags and configuration settings here.
+	lintCmd := &cobra.Command{
+		Use:   "lint",
+		Short: "Runs the linter checks",
+		Long:  lintLongDesc,
+		Run: func(cmd *cobra.Command, args []string) {
+			// pass lint checks into constructor
+			evmosChecker := checker.NewChecker()
+			err := evmosChecker.Run()
+			if err != nil {
+				log.Fatal().Err(err).Msg("failed to run evmos-checker")
+			}
+		},
+	}
 
-	return LintCmd
+	return lintCmd
 }
--- cmd/root.go
+++ cmd/root.go
@@ -12,26 +12,33 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "evmos-checker",
-	Short: "A collection of Evmos related checks",
-	Long: `Evmos Checker | Malte Herrmann | Evmos Core Team
-
-This tool is a collection of checks that are run during development of the Evmos core protocol.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		evmosChecker := checker.NewChecker()
-
-		err := evmosChecker.Run()
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to run evmos-checker")
-		}
-	},
-}
-
 // NewRootCmd returns the root command of the evmos-checker tool.
 func NewRootCmd() *cobra.Command {
-	rootCmd.AddCommand(lintchecks.NewLintCmd())
+	rootCmd := newRootCmdRaw()
+	lintCmd := lintchecks.NewLintCmd()
+	rootCmd.AddCommand(lintCmd)
+
+	return rootCmd
+}
+
+// newRootCmdRaw returns the root command of the evmos-checker tool without any subcommands or flags configured.
+func newRootCmdRaw() *cobra.Command {
+	// rootCmd represents the base command when called without any subcommands
+	rootCmd := &cobra.Command{
+		Use:   "evmos-checker",
+		Short: "A collection of Evmos related checks",
+		Long: `Evmos Checker | Malte Herrmann | Evmos Core Team
+
+This tool is a collection of checks that are run during development of the Evmos core protocol.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			evmosChecker := checker.NewChecker()
+
+			err := evmosChecker.Run()
+			if err != nil {
+				log.Fatal().Err(err).Msg("failed to run evmos-checker")
+			}
+		},
+	}
 
 	return rootCmd
 }
@@ -40,6 +47,7 @@ func NewRootCmd() *cobra.Command {
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	root := NewRootCmd()
+
 	err := root.Execute()
 	if err != nil {
 		os.Exit(1)
